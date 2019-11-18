28ff527aa3d533e003f7412041aabcbb90cc1822
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-11-17 20:16:37 -0800

    changed port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const server = require('./server.js');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const PORT = process.env.PORT || 4000;
+
 // connect to mongoDb cloud
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
@@ -14,7 +16,6 @@ connection.once('open', () => {
     console.log('MongoDb connection establish')
 })
 
-const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
