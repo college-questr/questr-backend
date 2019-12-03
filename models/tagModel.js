a8b7abd44e3576ac60cbcdc0dc923603656ee123
kiamco <kiamcoo@gmail.com>
2019-12-02 19:04:13 -0800

    start from the beginnig

--- models/tagModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
-
-const tagSchema = new Schema({
-    question_id: String,
-    tag: String
-});
-
-module.exports = mongoose.model("Tags", tagSchema);
